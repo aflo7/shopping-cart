50b4a56d0a999815fceaf85d3d6a0d223b8ff805
Andres Flores <andresflo883@gmail.com>
2023-07-05 03:32:38 -0400

    added iphone choices

--- src/components/Product_iPhone.jsx
+++ src/components/Product_iPhone.jsx
@@ -1,66 +1,115 @@
-import "../css/iPhone.css"
+import "../css/product-iphone.css"
 
 const phoneInfo = [
     {
         name: "iPhone 14 Pro",
         desc: "The ultimate iPhone.",
-        price: "$999",
-        size: '6.7" or 6.1"'
+        price: 999,
+        size: '6.7" or 6.1"',
+        img: "https://www.apple.com/v/iphone/home/bo/images/overview/compare/compare_iphone_14_pro__cjmfbiggqhpy_large.jpg"
     },
     {
         name: "iPhone 14",
         desc: "A total powerhouse.",
-        price: "$799",
-        size: '6.7" or 6.1"'
+        price: 799,
+        size: '6.7" or 6.1"',
+        img: "https://www.apple.com/v/iphone/home/bo/images/overview/compare/compare_iphone_14__ct4sjk962pea_large.jpg"
     },
     {
         name: "iPhone 13",
         desc: "As amazing as ever.",
-        price: "$599",
-        size: '6.1" or 5.4"'
+        price: 599,
+        size: '6.1" or 5.4"',
+        img: "https://www.apple.com/v/iphone/home/bo/images/overview/compare/compare_iphone_13__fqzwhmfmroey_large.jpg"
     },
     {
         name: "iPhone SE",
         desc: "Serious power. Serious value.",
-        price: "$429",
-        size: '4.7"'
+        price: 429,
+        size: '4.7"',
+        img: "https://www.apple.com/v/iphone/home/bo/images/overview/compare/compare_iphone_se__d5blqx1pgymq_large.jpg"
     }
 ]
 
 function Product_iPhone() {
     return (
-        <div className="iphone">
+        <div className="product-iphone-wrapper">
             <div
                 style={{
                     display: "flex",
                     flexDirection: "column",
                     alignItems: "center",
-                    gap: "20px",
-                    marginTop: "20px"
+                    gap: "10px",
+                    marginTop: "40px"
                 }}
             >
-                <h4>iPhone 14</h4>
-                <h1>Wonderfull.</h1>
+                <p
+                    style={{
+                        textAlign: "center",
+                        fontSize: "1.6rem",
+                        fontWeight: "bold"
+                    }}
+                >
+                    iPhone 14
+                </p>
+                <p
+                    style={{
+                        textAlign: "center",
+                        fontSize: "3rem",
+                        fontWeight: "bold"
+                    }}
+                >
+                    Wonderfull.
+                </p>
                 <p>From $799 or $33.29/mo. for 24 mo. before trade‑in</p>
                 <p className="blue-text">Learn more &#62;</p>
             </div>
-            <img
-                src="https://www.apple.com/v/iphone/home/bo/images/overview/hero/hero_iphone_14__de41900yuggi_large.jpg"
-                width={"100%"}
-                style={{ marginTop: "20px" }}
-            ></img>
 
             <div>
-                <h1>Which iPhone is right for you?</h1>
-                <div>
-                    {phoneInfo.map((item, i) => {
-                        return (
-                            <div key={i}>
-                                <h4>{item.name}</h4>
-                                <button>Add to cart</button>
-                            </div>
-                        )
-                    })}
+                <img
+                    src="https://www.apple.com/v/iphone/home/bo/images/overview/hero/hero_iphone_14__de41900yuggi_large.jpg"
+                    width={"100%"}
+                ></img>
+            </div>
+            <div style={{ display: "flex", justifyContent: "center" }}>
+                <div className="phone-content">
+                    <p
+                        style={{
+                            textAlign: "center",
+                            fontSize: "2rem",
+                            fontWeight: "bold"
+                        }}
+                    >
+                        Which iPhone is right for you?
+                    </p>
+                    <div className="phone-info-wrapper">
+                        {phoneInfo.map((item, i) => {
+                            return (
+                                <div className="phone-info" key={i}>
+                                    <img
+                                        src={item.img}
+                                        alt={item.name}
+                                        height={"250px"}
+                                    />
+                                    <p
+                                        style={{
+                                            fontWeight: "bold",
+                                            fontSize: "1.2rem"
+                                        }}
+                                    >
+                                        {item.name}
+                                    </p>
+                                    <p>{item.desc}</p>
+                                    <p>${item.price}</p>
+
+                                    <button>Add to cart</button>
+                                    <p className="blue-text">
+                                        Learn more &#62;
+                                    </p>
+                                </div>
+                            )
+                        })}
+                    </div>
                 </div>
             </div>
         </div>
