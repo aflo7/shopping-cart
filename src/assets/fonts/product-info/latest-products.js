6bdbe1afcec515e18dc6a35b624a9a5924ea9d4f
Andres Flores <andresflo883@gmail.com>
2023-07-17 23:08:57 -0400

    import latestProducts

---
+++ src/assets/fonts/product-info/latest-products.js
@@ -0,0 +1,40 @@
+
+const latestProducts = [
+  {
+      name: "IPHONE 14 PRO",
+      desc: "Pro. Beyond.",
+      price: "From $999 or $41.62/mo. for 24 months before trade-in"
+  },
+  {
+      name: "MACBOOK AIR 15”",
+      desc: "Impressively big. Impossibly thin.",
+      price: "From $1299 or $108.25/mo. for 12 months"
+  },
+  {
+      name: "MAC STUDIO",
+      desc: "Supercharged by M2 Max and M2 Ultra.",
+      price: "From $1999 or $166.58/mo. for 12 months"
+  },
+  {
+      name: "LIMITED TIME OFFER",
+      desc: "Save on Mac or iPad for College.",
+      price: "Plus get a gift card up to $150"
+  },
+  {
+      name: "APPLE WATCH SERIES 8",
+      desc: "A healthy leap forward.",
+      price: "Plus get a gift card up to $150"
+  },
+  {
+      name: "IPAD AIR",
+      desc: "Light. Bright. Full of might.",
+      price: "From $599 or $49.91/mo. per month for 24 months"
+  },
+  {
+      name: "IPHONE 14",
+      desc: "Wonderfull.",
+      price: "From $799 or $33.29/mo.per month for 24 mo.months before trade-in"
+  }
+]
+
+export default latestProducts
--- src/components/Store.jsx
+++ src/components/Store.jsx
@@ -6,44 +6,7 @@ import storeIpad from "../images/storeipad.png"
 import { Link } from "react-router-dom"
 import { motion } from "framer-motion"
 import { useState, useEffect } from "react"
-
-const latestProducts = [
-    {
-        name: "IPHONE 14 PRO",
-        desc: "Pro. Beyond.",
-        price: "From $999 or $41.62/mo. for 24 months before trade-in"
-    },
-    {
-        name: "MACBOOK AIR 15”",
-        desc: "Impressively big. Impossibly thin.",
-        price: "From $1299 or $108.25/mo. for 12 months"
-    },
-    {
-        name: "MAC STUDIO",
-        desc: "Supercharged by M2 Max and M2 Ultra.",
-        price: "From $1999 or $166.58/mo. for 12 months"
-    },
-    {
-        name: "LIMITED TIME OFFER",
-        desc: "Save on Mac or iPad for College.",
-        price: "Plus get a gift card up to $150"
-    },
-    {
-        name: "APPLE WATCH SERIES 8",
-        desc: "A healthy leap forward.",
-        price: "Plus get a gift card up to $150"
-    },
-    {
-        name: "IPAD AIR",
-        desc: "Light. Bright. Full of might.",
-        price: "From $599 or $49.91/mo. per month for 24 months"
-    },
-    {
-        name: "IPHONE 14",
-        desc: "Wonderfull.",
-        price: "From $799 or $33.29/mo.per month for 24 mo.months before trade-in"
-    }
-]
+import latestProducts from "../assets/fonts/product-info/latest-products"
 
 function Store() {
     const [saveColor, setSaveColor] = useState("rgb(9, 94, 192)")
@@ -158,6 +121,7 @@ function Store() {
                     </div>
                 </div>
             </div>
+            
             <div className="store-wrapper">
                 <div className="store-content">
                     <div className="quick-link-wrapper">
